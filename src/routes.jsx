5714166993038d2f904dddb539eddbf24f25889e
dariom25 <dario@maciey.de>
2024-04-05 16:53:16 +0200

    Add routes.jsx + implement routes array

---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import App from "./App/App";
+
+const routes = [
+    {
+        path: "/",
+        element: <App/>
+        // hier fehlt noch ein error element
+    },
+    {
+        path: ""
+    }
+]
+
+export default routes;
