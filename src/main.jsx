41a2a5f16010d85607776b5456749ac898dbbef8
dariom25 <dario@maciey.de>
2024-04-11 17:52:35 +0200

    Change files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App/App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./Homepage/Homepage";
+import Shop from "./Shop/Shop";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { 
+    path: "/", 
+    element: <App /> //in diesem element wird die homepage direkt geladen --> 
+  },
+  {
+    path: "homepage",
+    element: <Homepage />,
+  },
+  {
+    path: "shop",
+    element: <Shop />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
