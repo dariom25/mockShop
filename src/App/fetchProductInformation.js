081cd1250caa75c15d7009e972becc081d23ed2e
dariom25 <dario@maciey.de>
2024-04-12 14:31:18 +0200

    Refactor to custom hook

--- src/App/fetchProductInformation.js
+++
@@ -1,9 +0,0 @@
-const fetchProductInformation = async (url) => {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-        throw new Error (`HTTP error: Status ${response.status}`)
-    }
-}
-
-export default fetchProductInformation;
