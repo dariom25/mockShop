e4e215887db8768f3333b2d31228dbdf035d0593
dariom25 <dario@maciey.de>
2024-04-13 21:09:14 +0200

    Fix bugs

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,10 +1,12 @@
 import "./Product.css";
 
-const Product = (title, price, description, image, id) => {
+const Product = ({title, price, description, image, id}) => {
   return (
     <div className="product-container">
       <img src={image} alt={title} />
       <h3>{title}</h3>
+      <p>{description}</p>
+      <p>{price}</p>
     </div>
   );
 };
