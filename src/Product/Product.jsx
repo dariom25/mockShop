abb9da471a36c976e9d007f204bca49a18932c15
dariom25 <dario@maciey.de>
2024-04-14 13:30:03 +0200

    Add number input and button

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,12 +1,26 @@
 import "./Product.css";
 
-const Product = ({title, price, description, image, id}) => {
+const Product = ({ title, price, description, image, id, quantity, addHandler }) => {
   return (
     <div className="product-container">
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>{description}</p>
       <p>{price}</p>
+      <div className="add-to-cart-container">
+        <div className="quantity-container">
+          <label htmlFor="quantity">Quantity:</label>
+          <input
+            value={quantity}
+            min="1"
+            className="quantity"
+            type="number"
+            name="quantity"
+            id="quantity"
+          />
+        </div>
+        <button onClick={addHandler} className="add-button">Add to cart</button>
+      </div>
     </div>
   );
 };
