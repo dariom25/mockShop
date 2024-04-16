359d69c034815e65d111277ea35237f84da4267a
dariom25 <dario@maciey.de>
2024-04-16 19:49:20 +0200

    Add Cart component to Navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Cart from "../Cart/Cart";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       <Link to="/shop">Shop</Link>
       <Link to="/homepage">Homepage</Link>
+      <Cart />
     </div>
   );
 };
