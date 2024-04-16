b6c6b37844f61f450c38242add35a52464c8c595
dariom25 <dario@maciey.de>
2024-04-16 14:59:12 +0200

    Pass quantity value to updateCart function

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import "./Product.css";
 
-const Product = ({ title, price, description, image, id, quantity, addHandler }) => {
+const Product = ({
+  title,
+  price,
+  description,
+  image,
+  id,
+  updateCart,
+}) => {
+  const [amount, setAmount] = useState(0);
+
+  const handleUpdateCart = () => {
+    updateCart(id, amount);
+  };
+
   return (
     <div className="product-container">
       <img src={image} alt={title} />
@@ -11,15 +25,18 @@ const Product = ({ title, price, description, image, id, quantity, addHandler })
         <div className="quantity-container">
           <label htmlFor="quantity">Quantity:</label>
           <input
-            value={quantity}
+            value={amount}
             min="1"
             className="quantity"
             type="number"
             name="quantity"
             id="quantity"
+            onChange={(event) => setAmount(event.target.value)}
           />
         </div>
-        <button onClick={addHandler} className="add-button">Add to cart</button>
+        <button onClick={handleUpdateCart} className="add-button">
+          Add to cart
+        </button>
       </div>
     </div>
   );
