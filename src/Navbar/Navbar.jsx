a8ba2adb7e88fe5c80cd9714cd52a158650bbaad
dariom25 <dario@maciey.de>
2024-04-17 15:18:30 +0200

    Add Outlet component

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Cart from "../Cart/Cart";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <Link to="/shop">Shop</Link>
-      <Link to="/homepage">Homepage</Link>
-      <Cart />
+    <div>
+      <div className="navbar-container">
+        <h1>Mocks Schmock Shop</h1>
+        <div className="link-container">
+          <Link to="/shop">Shop</Link>
+          <Link to="/homepage">Homepage</Link>
+          <Cart />
+        </div>
+      </div>
+      <div className="outlet-container">
+        <Outlet />
+      </div>
     </div>
   );
 };
