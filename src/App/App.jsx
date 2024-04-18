bf40b1860d4e4f585ae97e7729aa0e69615cba02
dariom25 <dario@maciey.de>
2024-04-18 19:11:48 +0200

    Move useProductInformation hook out of App component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,28 +3,29 @@ import Shop from "../Shop/Shop";
 import { Navbar } from "../Navbar/Navbar";
 import { useState, useEffect } from "react";
 
-function App() {
-  const useProductInformation = () => {
-    const [productInformation, setProductInformation] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+const useProductInformation = () => {
+  const [productInformation, setProductInformation] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-      fetch("https://fakestoreapi.com/products", { mode: "cors" })
-        .then((response) => {
-          if (response.status >= 400) {
-            throw new Error("server error");
-          }
-          return response.json();
-        })
-        .then((response) => {
-          setProductInformation(response);
-        })
-        .catch((error) => setError(error))
-        .finally(() => setLoading(false));
-    }, []);
-    return { productInformation, error, loading };
-  };
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => {
+        setProductInformation(response);
+      })
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+  return { productInformation, error, loading };
+};
+
+function App() {
 
   const { productInformation, error, loading } = useProductInformation();
 
