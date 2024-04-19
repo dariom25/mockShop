18254faa6d8f161f0c59b7dbf1e0101bfc666b05
dariom25 <dario@maciey.de>
2024-04-19 12:10:00 +0200

    Change layout of Homepage

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -4,9 +4,8 @@ import "./Homepage.css";
 const Homepage = () => {
   return (
     <div className="homepage-container">
-      <img src="src/assets/mocksTitlePicture.webp" alt="random image" />
       <Link to="shop">
-        <span className="link-shop">Go to the shop</span>
+        <img src="src/assets/mocksTitlePicture.webp" alt="random image" />
       </Link>
     </div>
   );
