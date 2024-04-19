184739def9ca415855dab79e729251bd9b7d11e7
dariom25 <dario@maciey.de>
2024-04-19 09:29:33 +0200

    Restructure component

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,15 +1,13 @@
 import { Link } from "react-router-dom";
+import "./Homepage.css";
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Welcome to Mocks Online-Schmock Shop</h1>
-      <p>
-        Here stands a text which describes why the shop is so special and why
-        you should buy something
-      </p>
+    <div className="homepage-container">
       <img src="src/assets/mocksTitlePicture.webp" alt="random image" />
-      <Link to="/">Go to the shop</Link>
+      <Link to="shop">
+        <span className="link-shop">Go to the shop</span>
+      </Link>
     </div>
   );
 };
